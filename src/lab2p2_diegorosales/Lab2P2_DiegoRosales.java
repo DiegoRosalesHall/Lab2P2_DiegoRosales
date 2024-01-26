591afb038df447af9c46729f7a247af5044761ae
DiegoRosalesHall <diegogerardorhall2@Gmail.com>
2024-01-26 14:34:25 -0600

    Validacion de usuarios

--- src/lab2p2_diegorosales/Lab2P2_DiegoRosales.java
+++ src/lab2p2_diegorosales/Lab2P2_DiegoRosales.java
@@ -1,9 +1,11 @@
 package lab2p2_diegorosales;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Lab2P2_DiegoRosales {
 public static Scanner mcgregor = new Scanner(System.in);
+public static Scanner mcgregorstring = new Scanner(System.in);
     public static void main(String[] args) {
         int a=1;
         while (a==1){
@@ -13,7 +15,52 @@ public static Scanner mcgregor = new Scanner(System.in);
         int opcionMenu1 = mcgregor.nextInt();
                 
         if(opcionMenu1 ==1){
-            System.out.println("Ingrese su nombre: ");
+            ArrayList<Usuarios> usuariosValidos = new ArrayList<>();
+            
+            Usuarios estudiante = new Usuarios("Luis","Luisitoelpillo","estudiante");
+            usuariosValidos.add(estudiante);
+            Usuarios profesor = new Usuarios("Josue","Josuesito","profesor");
+            usuariosValidos.add(profesor);
+            Usuarios bibliotecario = new Usuarios("Diego","2305","bibliotecario");
+            usuariosValidos.add(bibliotecario);
+            
+            System.out.println("Ingrese su nombre de usuario: ");
+            String nombreUsuario = mcgregorstring.nextLine();
+            System.out.println("Ingrese su contraseña: ");
+            String contrasenaUsuario = mcgregorstring.nextLine();
+            int nUsuario = 0;
+            int contValidez = 0;
+        
+            for (int i = 0; i < usuariosValidos.size(); i++) {
+                if(usuariosValidos.get(i).getNombreUsuario().equals(nombreUsuario) && usuariosValidos.get(i).getContrasenaUsuario().equals(contrasenaUsuario)){
+                    contValidez++;
+                  
+                }
+                
+              
+                
+                if(contValidez==1){
+                    System.out.println("Valido");
+                    nUsuario=i;
+                    break;
+                }
+                  else {
+                    
+                    contValidez=0;
+                }
+                
+            }
+            
+            if(contValidez==1){
+                System.out.println("Has iniciado sesion con exito! Bienvenido "+usuariosValidos.get(nUsuario).getNombreUsuario());
+            }
+            
+            else{
+                System.out.println("El usuario/contraseña ingresado no es valido");
+            }
+            
+            
+            
         }// Inicio de sesion
         
         
--- src/lab2p2_diegorosales/Usuarios.java
+++ src/lab2p2_diegorosales/Usuarios.java
@@ -1,5 +1,41 @@
 package lab2p2_diegorosales;
 
 public class Usuarios {
+    String nombreUsuario;
+    String contrasenaUsuario;
+    String tipoUsuario;
     
+    public Usuarios(String nombreUsuario, String contrasenaUsuario, String tipoUsuario){
+        this.nombreUsuario = nombreUsuario;
+        this.contrasenaUsuario = contrasenaUsuario;
+        this.tipoUsuario = tipoUsuario;
+    }
+    
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getContrasenaUsuario() {
+        return contrasenaUsuario;
+    }
+
+    public void setContrasenaUsuario(String contrasenaUsuario) {
+        this.contrasenaUsuario = contrasenaUsuario;
+    }
+
+    public String getTipoUsuario() {
+        return tipoUsuario;
+    }
+
+    public void setTipoUsuario(String tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
+    }
+    
+    
+  
 }
